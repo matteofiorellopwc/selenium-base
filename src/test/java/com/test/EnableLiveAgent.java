23f46c85cf2034bc6b4d200ded1f602c527af191
matteo <matteo.fiorello@it.pwc.com>
2016-11-11 11:49:52 +0100

    change test

--- src/test/java/com/test/EnableLiveAgent.java
+++ src/test/java/com/test/EnableLiveAgent.java
@@ -1,6 +1,8 @@
 package com.test;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
 public class EnableLiveAgent extends AbstractBaseTest {
@@ -8,11 +10,16 @@ public class EnableLiveAgent extends AbstractBaseTest {
 	@Test(groups = { "feauture1" })
 	public void testEnableLiveAgent() throws Exception {
 		super.doLogin();
-		driver.findElement(By.id("setupLink")).click();
-		driver.findElement(By.id("setupSearch")).clear();
-		driver.findElement(By.id("setupSearch")).sendKeys("live");
-		driver.findElement(By.id("LiveAgentSettings_font")).click();
-		driver.findElement(By.id("j_id0:j_id15:thePageBlock:generalSection:generalSectionItem:togglePref")).click();
+		WebDriverWait wait = new WebDriverWait(driver, 10);
+
+		wait.until(ExpectedConditions.elementToBeClickable(By.id("userNavLabel"))).click();
+		wait.until(ExpectedConditions.elementToBeClickable(By.linkText("Setup"))).click();
+		wait.until(ExpectedConditions.elementToBeClickable(By.id("setupSearch"))).clear();
+		wait.until(ExpectedConditions.elementToBeClickable(By.id("setupSearch"))).sendKeys("live");
+		wait.until(ExpectedConditions.elementToBeClickable(By.id("LiveAgentSettings_font"))).click();
+		wait.until(ExpectedConditions
+				.elementToBeClickable(By.id("j_id0:j_id15:thePageBlock:generalSection:generalSectionItem:togglePref")))
+				.click();
 		driver.findElement(By.id("j_id0:j_id15:thePageBlock:j_id25:save")).click();
 		driver.findElement(By.id("j_id0:j_id15:thePageBlock:j_id25:save")).click();
 
